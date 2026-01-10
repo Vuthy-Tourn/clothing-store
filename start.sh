fbc73a0beb7662ea90ef265ecae15b908d39b1b5
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-01-10 18:58:05 +0700

    Removed: Caddyfile

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
-# Run FrankenPHP as the main process on port 8080
+# Start FrankenPHP as main process
+# Serve everything on 0.0.0.0:8080
 frankenphp -S 0.0.0.0:8080 -c /etc/frankenphp/frankenphp.ini
